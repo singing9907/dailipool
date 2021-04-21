35dd2a3bd09c9f0b4cc6cb3b0ed66a11f44417d3
singing9907 <43148219+singing9907@users.noreply.github.com>
2021-04-21 09:03:10 +0800

    Update web_fuzz.go

--- pkg/getter/web_fuzz.go
+++ pkg/getter/web_fuzz.go
@@ -5,8 +5,8 @@ import (
 	"log"
 	"sync"
 
-	"github.com/back20/proxypool/pkg/proxy"
-	"github.com/back20/proxypool/pkg/tool"
+	"github.com/singing9907/proxypool/pkg/proxy"
+	"github.com/singing9907/proxypool/pkg/tool"
 )
 
 // Add key value pair to creatorMap(string → creator) in base.go
